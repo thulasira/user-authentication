86b91a29398415d7535d89899b6f0a0552b2ca31
Thulasiraman <thulasiraman@bicsglobal.com>
2024-06-14 15:39:56 +0530

    updated maxduriation

--- index.js
+++ index.js
@@ -11,16 +11,8 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  // Set CORS headers
-  res.header("Access-Control-Allow-Origin", process.env.FRONTEND_URL); // Replace with your frontend domain
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials (cookies, etc.)
 
-  // Pass to next layer of middleware
-  next();
-});
+
 app.use("/api", authRoute);
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
