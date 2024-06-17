872fd6edfcc6198369dc1966b582cec00f32512d
Thulasiraman <thulasiraman@bicsglobal.com>
2024-06-17 15:03:45 +0530

    updated connection file

--- database/db.js
+++ database/db.js
@@ -1,20 +1,16 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
+const mySecret = process.env.MONGO_URI || 'mongodb+srv://bicstest007:bFtDNWvNlL12NrKp@cluster0.59hp8ul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const Connection = async () => {
   try {
-    const uri = process.env.MONGO_URI || 'mongodb+srv://bicstest007:bFtDNWvNlL12NrKp@cluster0.59hp8ul.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-    if (!uri) {
-      throw new Error('MONGO_URI environment variable not defined');
-    }
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Connected to MongoDB');
-  } catch (error) {
-    console.error('Error connecting to MongoDB', error);
-    process.exit(1);
+    await mongoose.connect(mySecret, {
+    })
+    console.log("db connected")
+    
   }
-};
+  catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = Connection;
